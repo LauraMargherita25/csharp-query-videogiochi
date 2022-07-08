695bf5ce27181166ee81effe00074bc032f8a0b2
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-07-08 14:44:16 +0200

    Query 1-5

--- csharp-query-videogiochi/query.sql
+++ csharp-query-videogiochi/query.sql
@@ -2,17 +2,28 @@
 -- 
 -- ```
 -- 1- Selezionare tutte le software house americane (3)
--- 
+
+SELECT * FROM software_houses WHERE software_houses.country = 'United States';
+
 -- 2- Selezionare tutti i giocatori della città di 'Rogahnland' (2)
--- 
+
+SELECT * FROM players WHERE players.city = 'Rogahnland';
+
 -- 3- Selezionare tutti i giocatori il cui nome finisce per "a" (220)
--- 
+
+SELECT * FROM players WHERE players.name  LIKE '%a';
+ 
 -- 4- Selezionare tutte le recensioni scritte dal giocatore con ID = 800 (11)
--- 
+
+SELECT * FROM reviews WHERE reviews.player_id = 800;
+
 -- 5- Contare quanti tornei ci sono stati nell'anno 2015 (9)
--- 
+
+SELECT COUNT(id) FROM tournaments WHERE tournaments.year = 2015; 
+
 -- 6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
--- 
+
+ 
 -- 7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
 -- 
 -- 8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
